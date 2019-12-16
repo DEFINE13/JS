815de0bb8a01926fca204533eb0118db35b6e1e0
Define <ZIHABEN1199@i.ua>
2019-12-16 22:12:24 +0300

    constructor

--- Define.js
+++ Define.js
@@ -1,7 +1,15 @@
 
-const firstName='IVAN';
-const lastName='SENSEY';
-const space=' '
-const userfullName =firstName+space+lastName;
+const firstName = 'IVAN';
+const lastName = 'SENSEY';
+const space = ' '
+const userfullName = firstName + space + lastName;
 console.log(userfullName)
- 
+console.log(Object.prototype)
+const user = {
+    firstName: 'IVAN', lastName: 'SENSEY'
+}
+console.log (user)
+user.fullName = user.firstName + space + user.lastName 
+console.log(user)
+
+
