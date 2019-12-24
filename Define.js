0eaf2ce1a60418c657fd2bb2a0e8f13f6c374528
Define <ZIHABEN1199@i.ua>
2019-12-24 22:29:00 +0300

    Added iso

--- Define.js
+++ Define.js
@@ -8,8 +8,23 @@ console.log(Object.prototype)
 const user = {
     firstName: 'IVAN', lastName: 'SENSEY'
 }
-console.log (user)
-user.fullName = user.firstName + space + user.lastName 
-console.log(user)
 
+user.fullName = user.firstName + space + user.lastName
 
+console.log(InputEvent)
+
+const users = [
+    { firstName: 'user1', lastName: 'user1' }, { firstName: 'user9', lastName: 'user9' },
+]
+console.log(users)
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i].firstName)
+    users[i].firstName
+}
+function getUserFullName() {
+    for (let i = 0; i < users.length; i++) {
+        console.log(users[i].firstName)
+
+    }
+}
+getUserFullName ()
